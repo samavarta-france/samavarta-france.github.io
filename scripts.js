fda0ec32148b4d1d070d25e34c2e7ca1b903e1de
Igor <igor.rosenberg@gmail.com>
2026-09-07 23:06:15 +0200

    lean JS

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+    [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 });
